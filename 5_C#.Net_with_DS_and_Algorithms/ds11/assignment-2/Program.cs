0536c7f20cb1060c14529c97b9ac822c931295ef
Naveen0987 <119598963+Naveen0987@users.noreply.github.com>
2024-10-14 02:56:32 +0530

    Add files via upload

---
+++ 5_C#.Net_with_DS_and_Algorithms/ds11/assignment-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void FindSubsets(int[] weights, int m, List<int> currentSubset, int index)
+    {
+        if (m == 0)
+        {
+            Console.Write("{ ");
+            foreach (int weight in currentSubset)
+            {
+                Console.Write(weight + " ");
+            }
+            Console.WriteLine("}");
+            return;
+        }
+
+        if (m < 0 || index == weights.Length) return;
+
+        currentSubset.Add(weights[index]);
+        FindSubsets(weights, m - weights[index], currentSubset, index + 1);
+
+        currentSubset.RemoveAt(currentSubset.Count - 1);
+        FindSubsets(weights, m, currentSubset, index + 1);
+    }
+
+    static void Main()
+    {
+        int[] weights = { 5, 10, 12, 13, 15, 18 };
+        int m = 30;
+
+        List<int> currentSubset = new List<int>();
+        FindSubsets(weights, m, currentSubset, 0);
+    }
+}
---
+++ 5_C#.Net_with_DS_and_Algorithms/ds11/assignment-3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class NQueen
+{
+    static void Main()
+    {
+        int N = 4;
+        int[,] board = new int[N, N];
+
+        if (SolveNQueen(board, 0, N))
+            PrintBoard(board, N);
+        else
+            Console.WriteLine("Solution does not exist.");
+    }
+
+    static bool SolveNQueen(int[,] board, int col, int N)
+    {
+        if (col >= N)
+            return true;
+
+        for (int row = 0; row < N; row++)
+        {
+            if (IsSafe(board, row, col, N))
+            {
+                board[row, col] = 1;
+
+                if (SolveNQueen(board, col + 1, N))
+                    return true;
+
+                board[row, col] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    static bool IsSafe(int[,] board, int row, int col, int N)
+    {
+        for (int i = 0; i < col; i++)
+            if (board[row, i] == 1)
+                return false;
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i, j] == 1)
+                return false;
+
+        for (int i = row, j = col; i < N && j >= 0; i++, j--)
+            if (board[i, j] == 1)
+                return false;
+
+        for (int i = 0; i < row; i++)
+            if (board[i, col] == 1)
+                return false;
+
+        return true;
+    }
+
+    static void PrintBoard(int[,] board, int N)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+                Console.Write(board[i, j] + " ");
+            Console.WriteLine();
+        }
+    }
+}
