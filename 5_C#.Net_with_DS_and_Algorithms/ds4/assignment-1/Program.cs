2ed461839ce8b6aa94baa22908bebd842b008d0b
Naveen0987 <119598963+Naveen0987@users.noreply.github.com>
2024-10-14 02:50:27 +0530

    Add files via upload

---
+++ 5_C#.Net_with_DS_and_Algorithms/ds4/assignment-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FloydWarshall
+{
+    public static void Main()
+    {
+        int[,] graph = new int[,]
+        {
+            {0, 5, int.MaxValue, 10},
+            {int.MaxValue, 0, 3, int.MaxValue},
+            {int.MaxValue, int.MaxValue, 0, 1},
+            {int.MaxValue, int.MaxValue, int.MaxValue, 0}
+        };
+
+        int vertices = graph.GetLength(0);
+
+        for (int k = 0; k < vertices; k++)
+        {
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    if (graph[i, k] + graph[k, j] < graph[i, j])
+                    {
+                        graph[i, j] = graph[i, k] + graph[k, j];
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Shortest distances between all pairs of vertices:");
+        for (int i = 0; i < vertices; i++)
+        {
+            for (int j = 0; j < vertices; j++)
+            {
+                Console.Write(graph[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
