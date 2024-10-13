34815ad5c7c6e481578e374f6a93c876e2bdb230
Naveen0987 <119598963+Naveen0987@users.noreply.github.com>
2024-10-14 02:51:45 +0530

    Add files via upload

---
+++ 5_C#.Net_with_DS_and_Algorithms/ds5/assignment-2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Edge
+{
+    public int Source;
+    public int Destination;
+    public int Weight;
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        List<Edge> edges = new List<Edge>
+        {
+            new Edge { Source = 0, Destination = 1, Weight = 10 },
+            new Edge { Source = 0, Destination = 2, Weight = 6 },
+            new Edge { Source = 0, Destination = 3, Weight = 5 },
+            new Edge { Source = 1, Destination = 3, Weight = 15 },
+            new Edge { Source = 2, Destination = 3, Weight = 4 }
+        };
+
+        var shortestDistances = Dijkstra(edges, 4, 0);
+        Console.WriteLine("This is the Dijkstra Algorithm");
+
+        Console.WriteLine("Shortest distances from vertex 0:");
+        foreach (var distance in shortestDistances)
+        {
+            Console.WriteLine($"Vertex {shortestDistances.IndexOf(distance)}: {distance}");
+        }
+    }
+
+    static List<int> Dijkstra(List<Edge> edges, int vertices, int startVertex)
+    {
+        List<int> shortestDistances = new List<int>(new int[vertices]);
+        bool[] visited = new bool[vertices];
+
+        for (int i = 0; i < vertices; i++)
+        {
+            shortestDistances[i] = int.MaxValue;
+        }
+
+        shortestDistances[startVertex] = 0;
+
+        for (int i = 0; i < vertices - 1; i++)
+        {
+            int minDistanceVertex = -1;
+            int minDistance = int.MaxValue;
+
+            for (int j = 0; j < vertices; j++)
+            {
+                if (!visited[j] && shortestDistances[j] < minDistance)
+                {
+                    minDistance = shortestDistances[j];
+                    minDistanceVertex = j;
+                }
+            }
+
+            visited[minDistanceVertex] = true;
+
+            foreach (var edge in edges)
+            {
+                if (edge.Source == minDistanceVertex && !visited[edge.Destination] && shortestDistances[minDistanceVertex] + edge.Weight < shortestDistances[edge.Destination])
+                {
+                    shortestDistances[edge.Destination] = shortestDistances[minDistanceVertex] + edge.Weight;
+                }
+            }
+        }
+
+
+        return shortestDistances;
+        
+    }
+}
---
+++ 5_C#.Net_with_DS_and_Algorithms/ds5/assignment-3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Edge
+{
+    public int Source;
+    public int Destination;
+    public int Weight;
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Edge> edges = new List<Edge>
+        {
+            new Edge { Source = 0, Destination = 1, Weight = 10 },
+            new Edge { Source = 0, Destination = 2, Weight = 6 },
+            new Edge { Source = 0, Destination = 3, Weight = 5 },
+            new Edge { Source = 1, Destination = 3, Weight = 15 },
+            new Edge { Source = 2, Destination = 3, Weight = 4 }
+        };
+
+        var shortestDistances = BellmanFord(edges, 4, 0);
+
+        Console.WriteLine("Shortest distances from vertex 0:");
+        for (int i = 0; i < shortestDistances.Count; i++)
+        {
+            Console.WriteLine($"Vertex {i}: {shortestDistances[i]}");
+        }
+    }
+
+    static List<int> BellmanFord(List<Edge> edges, int vertices, int startVertex)
+    {
+        List<int> shortestDistances = new List<int>(new int[vertices]);
+
+        for (int i = 0; i < vertices; i++)
+        {
+            shortestDistances[i] = int.MaxValue;
+        }
+
+        shortestDistances[startVertex] = 0;
+
+        for (int i = 0; i < vertices - 1; i++)
+        {
+            foreach (var edge in edges)
+            {
+                if (shortestDistances[edge.Source] + edge.Weight < shortestDistances[edge.Destination])
+                {
+                    shortestDistances[edge.Destination] = shortestDistances[edge.Source] + edge.Weight;
+                }
+            }
+        }
+
+        foreach (var edge in edges)
+        {
+            if (shortestDistances[edge.Source] + edge.Weight < shortestDistances[edge.Destination])
+            {
+                Console.WriteLine("Negative-weight cycle detected");
+                return null;
+            }
+        }
+
+        return shortestDistances;
+    }
+}
